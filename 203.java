3390c8926e218a3cb274010b2ecb9ac248ea9124
edr <xieyu8840@gmail.com>
2022-11-10 23:14:21 +0800

    -

---
+++ 203.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode virtualIndex = new ListNode(-1, head);
+        ListNode pre = virtualIndex;
+        ListNode cur = head;
+
+        if (head == null) {
+            return head;
+        }
+
+        while (cur != null) {
+            if (cur.val == val) {
+                pre.next = cur.next;
+            } else {
+                pre = cur;
+            }
+            cur = cur.next;
+        }
+
+        return virtualIndex.next;
+    }
+}
