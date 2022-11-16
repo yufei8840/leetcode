d3fb23cb4e4c1d6ac68f477ddf2e35588fef3ad2
edr <xieyu8840@gmail.com>
2022-11-16 21:41:32 +0800

    -

---
+++ 160.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        int lenA = 0, lenB = 0;
+
+        while (curA != null) {
+            curA = curA.next;
+            lenA++;
+        }
+        while (curB != null) {
+            curB = curB.next;
+            lenB++;
+        }
+        curA = headA;
+        curB = headB;
+        if (lenB > lenA) {
+            int tmpLen = lenA;
+            lenA = lenB;
+            lenB = tmpLen;
+            ListNode tmpNode = curA;
+            curA = curB;
+            curB = tmpNode;
+        }
+        int gap = lenA - lenB;
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        while (curA != null) {
+            if (curA == curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+}
